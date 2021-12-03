47861d2c1e41c612883aeccf63705604bb4cd08a
007msr <80089662+007msr@users.noreply.github.com>
2021-12-04 00:01:10 +0530

    Add files via upload
    Break The Progression

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+
+      long long n,x;
+      cin>>n>>x;
+      cout<<(2*x)<<endl;
+  }
+  return 0;
+}

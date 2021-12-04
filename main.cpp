95662139df429f9673b0c73949d08cebf4f6de77
007msr <80089662+007msr@users.noreply.github.com>
2021-12-04 17:31:07 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n,m,k;
+  cin>>n>>m>>k;
+  if(m>=n && k>=n)
+    cout<<"yes"<<endl;
+  else
+    cout<<"no"<<endl;
+  return 0;
+}
